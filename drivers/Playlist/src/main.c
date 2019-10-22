1e0aa15c1ddc861cb19ee94da10ecbac9cd95f22
David Baumann <david_baumann@student.uml.edu>
2019-10-22 16:53:16 -0400

    added code to play songs
    adapted from Playfile driver

--- drivers/Playlist/src/main.c
+++ drivers/Playlist/src/main.c
@@ -16,22 +16,86 @@
 
 // void run_test();
 
+void *decoderLibrary = NULL;
+AUDIO_DECODER *audioDecoder = NULL;
+FILE *fp = NULL;
+char *errorString = "nothing to see here";
+int eCode = 0;
+
+void PlayerThread(void) {
+	eCode = DecodeAudio(decoderLibrary, audioDecoder, &errorString);
+}
+
+
 int main(char *parameters) {
     char* playlist_filename = parameters;
+    Playlist* h_playlist;
+    FILE* current_song;
 
-    Playlist* h_playlist = create_playlist_from_file(playlist_filename);
+    printf("Loading Decoder library\n");
+	decoderLibrary = LoadLibrary("audiodec");
+	if (!decoderLibrary) {
+		printf("Couldn't open library\n");
+		return S_ERROR;
+	}
 
+
+    // init a new playlist from the given file
+    h_playlist = create_playlist_from_file(playlist_filename);
     if (h_playlist == NULL) {
         printf("Error generating playlist from file '%s'", playlist_filename);
         return S_ERROR;
     }
 
-    // print out what's in the playlist
+    // play all songs in playlist
     while (h_playlist->current != NULL) {
-        printf("Song in list: %s\n", h_playlist->current->filename);
+        current_song = fopen(h_playlist->current->filename, "rb");
+	    if (!current_song) {
+		    printf("Couldn't open file '%s'\n", current_song);
+		    return S_ERROR;
+	    }
+
+        printf("Now Playing: %s\n", current_song->Identify(current_song,NULL,0));
+        RunLibraryFunction("METADATA",ENTRY_2,(int)current_song);
+
+        audioDecoder = CreateAudioDecoder(decoderLibrary, current_song, stdaudioout, NULL, auDecFGuess);
+        if (!audioDecoder) {
+            printf("Couldn't create audio decoder\n");
+            return S_ERROR;
+        }
+
+        StartTask(TASK_DECODER, PlayerThread);
+        Delay(100);
+        while (pSysTasks[TASK_DECODER].task.tc_State && pSysTasks[TASK_DECODER].task.tc_State != TS_REMOVED) {
+
+            printf("\r[%02ld:%02ld]",audioDecoder->cs.playTimeSeconds/60L,audioDecoder->cs.playTimeSeconds%60L);
+
+            Delay(250);
+            if (ioctl(stdin,IOCTL_TEST,NULL)) {
+                //character(s) available in the stdin buffer
+                char c = fgetc(stdin);
+                if (c=='q') {
+                    printf("\rQuit... \n");
+                    audioDecoder->cs.cancel = 1;
+                }
+            }
+
+            if (appFlags & APP_FLAG_QUIT) {
+                audioDecoder->cs.cancel = 1;
+            }
+
+            Delay(250);
+        }
+
+        // go to next song
+        fclose(current_song);
+        DeleteAudioDecoder(decoderLibrary, audioDecoder);
         h_playlist->current = h_playlist->current->next;
     }
+    // reset playlist to beginning
     h_playlist->current = h_playlist->head;
+
+	DropLibrary(decoderLibrary);
 
     destroy_playlist(&h_playlist);
     return S_OK;
