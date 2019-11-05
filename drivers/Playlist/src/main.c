cfe4a221fad3257ab15a3768c90f8cc3ce09a54c
Christian Patterson <fondu16@gmail.com>
2019-11-05 13:36:49 -0500

    (UNTESTED) Add play/pause, go to prev, restart song, and loop playlist

--- drivers/Playlist/src/main.c
+++ drivers/Playlist/src/main.c
@@ -3,14 +3,13 @@
 #include <vstypes.h>
 #include <stdlib.h>
 #include <string.h>
-#include <apploader.h>  // RunLibraryFunction etc
+#include <apploader.h> // RunLibraryFunction etc
 #include <timers.h>
 #include <libaudiodec.h>
 #include <vsostasks.h>
 #include <consolestate.h>
 #include <kernel.h>
 #include <unistd.h>
-
 
 #include "playlist.h"
 
@@ -26,23 +25,25 @@ char *errorString = "nothing to see here";
 int eCode = 0;
 
 void PlayerThread(void) {
-	eCode = DecodeAudio(decoderLibrary, audioDecoder, &errorString);
+    eCode = DecodeAudio(decoderLibrary, audioDecoder, &errorString);
 }
 
-
 int main(char *parameters) {
-    char* playlist_filename = parameters;
-    Playlist* h_playlist;
-    FILE* current_song;
+    char *playlist_filename = parameters;
+    Playlist *h_playlist;
+    FILE *current_song;
     char user_input;
     u_int16 quit_selected = FALSE;
+    u_int16 restart_song = FALSE;
+    u_int16 move_prev = FALSE;
+    int time_elapsed;
 
     printf("Loading Decoder library\n");
-	decoderLibrary = LoadLibrary("audiodec");
-	if (!decoderLibrary) {
-		printf("Couldn't open library\n");
-		return S_ERROR;
-	}
+    decoderLibrary = LoadLibrary("audiodec");
+    if (!decoderLibrary) {
+        printf("Couldn't open library\n");
+        return S_ERROR;
+    }
 
     // init a new playlist from the given file
     h_playlist = create_playlist_from_file(playlist_filename);
@@ -54,13 +55,13 @@ int main(char *parameters) {
     // play all songs in playlist
     while (h_playlist->current != NULL && quit_selected == FALSE) {
         current_song = fopen(h_playlist->current->filename, "rb");
-	    if (!current_song) {
-		    printf("Couldn't open file '%s'\n", current_song);
-		    return S_ERROR;
-	    }
+        if (!current_song) {
+            printf("Couldn't open file '%s'\n", current_song);
+            return S_ERROR;
+        }
 
-        printf("Now Playing: %s\n", current_song->Identify(current_song,NULL,0));
-        RunLibraryFunction("METADATA",ENTRY_2,(int)current_song);
+        printf("Now Playing: %s\n", current_song->Identify(current_song, NULL, 0));
+        RunLibraryFunction("METADATA", ENTRY_2, (int)current_song);
 
         audioDecoder = CreateAudioDecoder(decoderLibrary, current_song, stdaudioout, NULL, auDecFGuess);
         if (!audioDecoder) {
@@ -72,24 +73,48 @@ int main(char *parameters) {
         Delay(100);
         while (pSysTasks[TASK_DECODER].task.tc_State && pSysTasks[TASK_DECODER].task.tc_State != TS_REMOVED) {
 
-            printf("\r[%02ld:%02ld]",audioDecoder->cs.playTimeSeconds/60L,audioDecoder->cs.playTimeSeconds%60L);
+            printf("\r[%02ld:%02ld]", audioDecoder->cs.playTimeSeconds / 60L, audioDecoder->cs.playTimeSeconds % 60L);
 
             Delay(250);
-            if (ioctl(stdin,IOCTL_TEST,NULL)) {
+            if (ioctl(stdin, IOCTL_TEST, NULL)) {
                 //character(s) available in the stdin buffer
                 user_input = fgetc(stdin);
                 switch (user_input) {
-                    case 'S':
-                    case 's':
-                        printf("\rSkipping... \n");
-                        audioDecoder->cs.cancel = 1;
-                        break;
-                    case 'Q':
-                    case 'q':
-                        printf("Quitting...\n");
-                        audioDecoder->cs.cancel = 1;
-                        quit_selected = TRUE;
-                        break;
+                case 'S':
+                case 's':
+                    printf("\rSkipping... \n");
+                    audioDecoder->cs.cancel = 1;
+                    break;
+                case 'B':
+                case 'b':
+                    time_elapsed = audioDecoder->cs.playTimeSeconds;
+                    audioDecoder->cs.cancel = 1;
+
+                    if (time_elapsed < 3) {
+                        printf("Moving to previous...\n");
+                        move_prev = TRUE;
+                    }
+                    else {
+                        printf("Restarting song...\n");
+                        restart_song = TRUE;
+                    }
+
+                    break;
+                case 'Q':
+                case 'q':
+                    printf("Quitting...\n");
+                    audioDecoder->cs.cancel = 1;
+                    quit_selected = TRUE;
+                    break;
+                case 'P':
+                case 'p':
+                    audioDecoder->pause ^= 1;
+                    if (audioDecoder->pause) {
+                        printf("Paused\n");
+                    }
+                    else {
+                        printf("Playing\n");
+                    }
                 }
             }
 
@@ -102,22 +127,39 @@ int main(char *parameters) {
             Delay(250);
         }
 
-        // go to next song
         fclose(current_song);
         DeleteAudioDecoder(decoderLibrary, audioDecoder);
-        h_playlist->current = h_playlist->current->next;
+        if (restart_song) {
+            // Pointer remains at current song
+        }
+        else if (move_prev) {
+            // go to previous song
+            if (h_playlist->current->prev != NULL) {
+                h_playlist->current = h_playlist->current->prev;
+            }
+            else {
+                // at top, to move to tail
+                h_playlist->current = h_playlist->last;
+            }
+        }
+        else if (h_playlist->current->next != NULL) {
+            // go to next song
+            h_playlist->current = h_playlist->current->next;
+        }
+        else {
+            // reset playlist to beginning
+            h_playlist->current = h_playlist->head;
+        }
     }
-    // reset playlist to beginning
-    h_playlist->current = h_playlist->head;
 
-	DropLibrary(decoderLibrary);
+    DropLibrary(decoderLibrary);
 
     destroy_playlist(&h_playlist);
     return S_OK;
 }
 
 /* Test Code for Playlist
-void run_test() {
+void run_test()  {
     Playlist* h_my_playlist;
     Playlist_Entry* temp;
     printf("\n\nTesting Playlist functions...\n");
@@ -131,7 +173,7 @@ void run_test() {
     printf("Adding song3.mp3\n");
     add_song(h_my_playlist, "song3.mp3");
 
-    while (h_my_playlist->current != NULL) {
+    while (h_my_playlist->current != NULL)  {
         printf("Song in list: %s\n", h_my_playlist->current->filename);
         h_my_playlist->current = h_my_playlist->current->next;
     }
@@ -139,7 +181,7 @@ void run_test() {
 
     printf("Length of list: %d\n", h_my_playlist->length);
 
-    while (h_my_playlist->current != NULL) {
+    while (h_my_playlist->current != NULL)  {
         printf("Removing %s from list...\n", h_my_playlist->current->filename);
         temp = h_my_playlist->current->next;
         delete_song(h_my_playlist, h_my_playlist->current);
